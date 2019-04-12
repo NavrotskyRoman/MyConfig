6969f730fb39e58b2df5727a00f18b2e6b23b8ad
Navrotsky Roman <navrocky.roman@gmail.com>
2019-04-12 09:54:23 +0300

    Птн Апр 12 09:54:15 EEST 2019

--- polybar/polybar.sh
+++ polybar/polybar.sh
@@ -7,7 +7,17 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 2; done
 
 # Launch bar1 and bar2
-polybar default -r &
-polybar down -r &
+#polybar default -r &
+#polybar down -r &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload default &
+    MONITOR=$m polybar --reload down &
+  done
+else
+  polybar --reload default &
+  polybar --reload down &
+fi
+
 
 echo "Bars launched..."
